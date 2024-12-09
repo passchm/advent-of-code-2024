0072b4f621df582317b620172b38584ce67d5dfe
Pascal Schmid <175302205+passchm@users.noreply.github.com>
2024-12-09 16:18:48 +0100

    Add Guile solver for day 09 part 2

--- solvers/09/solve.scm
+++ solvers/09/solve.scm
@@ -1,9 +1,8 @@
 (use-modules ((ice-9 textual-ports) #:select (get-string-all)))
+(use-modules ((srfi srfi-1) #:select (fold)))
 
 (define input-data
   (call-with-input-file "input.txt" get-string-all))
-
-; (define input-data "2333133121414131402")
 
 ; Part 1
 
@@ -58,14 +57,64 @@
       (compact-filesystem
         (disk-map->filesystem-data disk-map)))))
 
-; (display (part-1 input-data))
-(write (part-1 input-data))
+(display (part-1 input-data))
 (newline)
 
 ; Part 2
 
+(define (find-free-space-of-size filesystem-data file-size)
+  (let loop ((i 0))
+    (if (>= (+ i file-size) (vector-length filesystem-data))
+      #f
+      (if (and (= (vector-ref filesystem-data i) -1)
+               (equal? (vector-copy filesystem-data i (+ i file-size))
+                       (make-vector file-size -1)))
+        i
+        (loop (+ i 1))))))
+
+(define (find-file-with-id-number filesystem-data id-number)
+  (let ((found-file-range
+          (let loop ((i 0) (file-range '()))
+            (if (>= i (vector-length filesystem-data))
+              file-range
+              (if (= (vector-ref filesystem-data i) id-number)
+                (loop (+ i 1) (cons i file-range))
+                (if (null? file-range)
+                  (loop (+ i 1) file-range)
+                  file-range))))))
+    (cons (car (reverse found-file-range))
+          (+ 1 (- (car found-file-range) (car (reverse found-file-range)))))))
+
+(define (move-file! filesystem-data file-size from-offset to-offset)
+  (let ((data (vector-copy filesystem-data from-offset (+ from-offset file-size))))
+    (vector-fill! filesystem-data -1 from-offset (+ from-offset file-size))
+    (vector-copy! filesystem-data to-offset data)))
+
+(define (move-file-left-if-possible! filesystem-data id-number)
+  (let* ((file-range (find-file-with-id-number filesystem-data id-number))
+         (file-offset (car file-range))
+         (file-size (cdr file-range))
+         (free-space-offset (find-free-space-of-size filesystem-data file-size)))
+    (when (and free-space-offset (< free-space-offset file-offset))
+      (move-file! filesystem-data file-size file-offset free-space-offset))))
+
+(define (max-in-list lst)
+  (fold (lambda (x y) (if (> x y) x y)) (car lst) (cdr lst)))
+
+(define (compact-filesystem-whole-files filesystem-data)
+  (let ((compact-data (vector-copy filesystem-data))
+        (max-id-number (max-in-list (vector->list filesystem-data))))
+    (for-each
+      (lambda (id-number)
+        (move-file-left-if-possible! compact-data id-number))
+      (iota (+ max-id-number 1) max-id-number -1))
+    compact-data))
+
 (define (part-2 input-data)
-  '())
+  (let ((disk-map (parse-input input-data)))
+    (calculate-filesystem-checksum
+      (compact-filesystem-whole-files
+        (disk-map->filesystem-data disk-map)))))
 
 (display (part-2 input-data))
 (newline)
