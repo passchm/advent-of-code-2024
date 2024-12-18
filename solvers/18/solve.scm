03be92f5db611e9002181ebcd071b73644d1b15f
Pascal Schmid <175302205+passchm@users.noreply.github.com>
2024-12-18 14:38:18 +0100

    Improve the Guile solver for day 18 by using breadth-first search

--- solvers/18/solve.scm
+++ solvers/18/solve.scm
@@ -1,7 +1,5 @@
 (use-modules ((ice-9 textual-ports) #:select (get-string-all)))
-
-(add-to-load-path (dirname (current-filename)))
-(use-modules ((dijkstra) #:select (dijkstra-find-length-of-best-paths)))
+(use-modules ((ice-9 q) #:select (make-q enq! deq! q-empty?)))
 
 (define input-data
   (call-with-input-file "input.txt" get-string-all))
@@ -18,6 +16,38 @@
               (string-trim-right input-data #\linefeed)
               #\linefeed))))
 
+(define (bfs-parents root-node discover-neighbor-nodes)
+  (let ((Q (make-q))
+        (explored (make-hash-table))
+        (parents (make-hash-table)))
+    (hash-set! explored root-node #t)
+    (enq! Q root-node)
+    (let loop ()
+      (if (q-empty? Q)
+        #f
+        (let* ((node (deq! Q))
+               (neighbor-nodes (discover-neighbor-nodes node)))
+          (for-each
+            (lambda (neighbor-node)
+              (when (not (hash-ref explored neighbor-node))
+                (hash-set! explored neighbor-node #t)
+                (hash-set! parents neighbor-node node)
+                (enq! Q neighbor-node)))
+            neighbor-nodes)
+          (loop))))
+    parents))
+
+(define (bfs-path root-node goal-node discover-neighbor-nodes)
+  (let ((parents (bfs-parents root-node discover-neighbor-nodes)))
+    (if (equal? root-node goal-node)
+      (list root-node)
+      (if (not (hash-ref parents goal-node))
+        #f
+        (let loop ((path '()) (current-node goal-node))
+          (if (equal? current-node root-node)
+            (cons root-node path)
+            (loop (cons current-node path) (hash-ref parents current-node))))))))
+
 (define (build-corrupted-grid corrupted-coordinates)
   (let ((grid (make-array #f grid-size grid-size)))
     (for-each (lambda (coord) (array-set! grid #t (cdr coord) (car coord)))
@@ -31,22 +61,22 @@
       ; up
       (if (and (array-in-bounds? corrupted-grid (1- (cdr coord)) (car coord))
                (not (array-ref corrupted-grid (1- (cdr coord)) (car coord))))
-        (cons 1 (cons (car coord) (1- (cdr coord))))
+        (cons (car coord) (1- (cdr coord)))
         '())
       ; down
       (if (and (array-in-bounds? corrupted-grid (1+ (cdr coord)) (car coord))
                (not (array-ref corrupted-grid (1+ (cdr coord)) (car coord))))
-        (cons 1 (cons (car coord) (1+ (cdr coord))))
+        (cons (car coord) (1+ (cdr coord)))
         '())
       ; left
       (if (and (array-in-bounds? corrupted-grid (cdr coord) (1- (car coord)))
                (not (array-ref corrupted-grid (cdr coord) (1- (car coord)))))
-        (cons 1 (cons (1- (car coord)) (cdr coord)))
+        (cons (1- (car coord)) (cdr coord))
         '())
       ; right
       (if (and (array-in-bounds? corrupted-grid (cdr coord) (1+ (car coord)))
                (not (array-ref corrupted-grid (cdr coord) (1+ (car coord)))))
-        (cons 1 (cons (1+ (car coord)) (cdr coord)))
+        (cons (1+ (car coord)) (cdr coord))
         '()))))
 
 (define (part-1 input-data)
@@ -55,10 +85,10 @@
             (list-head (parse-input input-data) 1024)))
         (start-coord '(0 . 0))
         (end-coord (cons (1- grid-size) (1- grid-size))))
-    (dijkstra-find-length-of-best-paths
-      start-coord
-      end-coord
-      (lambda (coord) (possible-moves corrupted-grid coord)))))
+    (1- (length (bfs-path
+                  start-coord
+                  end-coord
+                  (lambda (coord) (possible-moves corrupted-grid coord)))))))
 
 (display (part-1 input-data))
 (newline)
@@ -77,7 +107,7 @@
         #f
         (begin
           (array-set! corrupted-grid #t (cdr (car corrupted-bytes)) (car (car corrupted-bytes)))
-          (if (not (dijkstra-find-length-of-best-paths
+          (if (not (bfs-path
                      start-coord
                      end-coord
                      (lambda (coord) (possible-moves corrupted-grid coord))))
