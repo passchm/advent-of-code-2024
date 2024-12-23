9e540e1f0ae4458ef4f37146bef3cc2f55836efc
Pascal Schmid <175302205+passchm@users.noreply.github.com>
2024-12-23 02:21:06 +0100

    Add Guile solver for day 22 part 2

--- solvers/22/solve.scm
+++ solvers/22/solve.scm
@@ -37,8 +37,77 @@
 
 ; Part 2
 
+(define (simulate-sequences initial-secret-number steps)
+  (let loop ((step 1)
+             (prev-price (euclidean-remainder initial-secret-number 10))
+             (curr-number (evolve-secret-number initial-secret-number))
+             (prev-diff #f)
+             (prev-prev-diff #f)
+             (prev-prev-prev-diff #f)
+             (diffs '()))
+    (if (= step steps)
+      (list-tail (reverse diffs) 3)
+      (let ((curr-price (euclidean-remainder curr-number 10)))
+        (loop (1+ step)
+              curr-price
+              (evolve-secret-number curr-number)
+              (- curr-price prev-price)
+              prev-diff
+              prev-prev-diff
+              (cons (list
+                      curr-price
+                      prev-prev-prev-diff
+                      prev-prev-diff
+                      prev-diff
+                      (- curr-price prev-price))
+                    diffs))))))
+
+(define (build-sequence-to-price-table initial-secret-number steps)
+  (let ((table (make-hash-table))
+        (sequences (simulate-sequences initial-secret-number steps)))
+    (for-each (lambda (seq)
+                ; Only the first occurrence of the sequence matters
+                (when (not (hash-ref table (cdr seq)))
+                  (hash-set! table (cdr seq) (car seq))))
+              sequences)
+    table))
+
+(define (build-all-sequences-set sequence-to-price-tables)
+  (let ((all-sequences-set (make-hash-table)))
+    (for-each (lambda (table)
+                (hash-for-each
+                  (lambda (key value) (hash-set! all-sequences-set key #t))
+                  table))
+              sequence-to-price-tables)
+    (hash-map->list (lambda (key value) key) all-sequences-set)))
+
+(define (find-best-sequence all-sequences-set sequence-to-price-tables)
+  (let loop ((best-price -inf.0) (best-sequence '()) (sequences all-sequences-set))
+    (if (null? sequences)
+      (cons best-price best-sequence)
+      (let ((price
+              (apply +
+                     (map (lambda (table) (hash-ref table (car sequences) 0))
+                          sequence-to-price-tables))))
+        (when (equal? (car sequences) '(-2 1 -1 3))
+          (map (lambda (table) (hash-ref table (car sequences) 0))
+               sequence-to-price-tables))
+        (if (> price best-price)
+          (loop price (car sequences) (cdr sequences))
+          (loop best-price best-sequence (cdr sequences)))))))
+
+; Attention: The test input for part 2 is different from the one for part 1!
+
 (define (part-2 input-data)
-  '())
+  (let* ((secret-numbers (parse-input input-data))
+         (sequence-to-price-tables
+           (map (lambda (secret-number)
+                  (build-sequence-to-price-table secret-number 2000))
+                secret-numbers))
+         (all-sequences-set
+           (build-all-sequences-set sequence-to-price-tables)))
+    (car
+      (find-best-sequence all-sequences-set sequence-to-price-tables))))
 
 (display (part-2 input-data))
 (newline)
